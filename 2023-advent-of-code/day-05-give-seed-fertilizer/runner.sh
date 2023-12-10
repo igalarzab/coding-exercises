3161b075203e9011ef84474a1537acc47d5995a4
Jose Galarza <igalarzab@gmail.com>
2023-12-10 14:45:52 +0900

    Renaming examples

--- 2023-advent-of-code/day-05-give-seed-fertilizer/runner.sh
+++ 2023-advent-of-code/day-05-give-seed-fertilizer/runner.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "--------- Running input-01 (part 1) ---------"
-cat input-01.txt | ./solution.py --part1 | diff output-01-part1.txt - && echo "OK"
+cat input-01.txt | ./solution.py --part1 | diff output-part1-01.txt - && echo "OK"
 
 echo "--------- Running input-02 (part 1) ---------"
-cat input-02.txt | ./solution.py --part1 | diff output-02-part1.txt - && echo "OK"
+cat input-02.txt | ./solution.py --part1 | diff output-part1-02.txt - && echo "OK"
 
 echo "--------- Running input-01 (part 2) ---------"
-cat input-01.txt | ./solution.py --part2 | diff output-01-part2.txt - && echo "OK"
+cat input-01.txt | ./solution.py --part2 | diff output-part2-01.txt - && echo "OK"
 
 echo "--------- Running input-02 (part 2) ---------"
-cat input-02.txt | ./solution.py --part2 | diff output-02-part2.txt - && echo "OK"
+cat input-02.txt | ./solution.py --part2 | diff output-part2-02.txt - && echo "OK"
 
-#echo "----------------- Benchmark -----------------"
-#cat input-02.txt | ./solution.py --benchmark
+echo "----------------- Benchmark -----------------"
+cat input-02.txt | ./solution.py --benchmark
